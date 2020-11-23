8a6c16d60c74d91f52a2ed1a89f23c46d828518c
Devnet PC <ecanogut@cisco.com>
2020-11-22 20:46:54 -0800

    Umbrella Script to get the latest malware domains for an IP address

--- LMDFIP.py
+++ LMDFIP.py
@@ -1,8 +1,8 @@
 import requests
 
-#Script developed by Emmanuel Cano. This scripts gets the first 10 last malwaresites related to an IP address
+#Script developed by Emmanuel Cano. This scripts gets the first 10 last malware domains related to an IP address.
 #It is important to generate and paste your Umbrella Investigate Token into authorization field 
-#In order to get more than 10 domains change the querystring limit
+#In order to get more than 10 domains change the querystring limit variable
 #More information can be found on https://docs.umbrella.com/investigate-api/docs/latest-malicious-domains-for-an-ip-1
 
 ipaddress = input("What is the ip address to be analyzed? ")
